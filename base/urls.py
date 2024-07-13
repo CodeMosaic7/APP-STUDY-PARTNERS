96890ed601b3ed4435a1b3de32a0a5c262bd7a96
Evie Frye <msmanika763348@gmail.com>
2024-07-13 21:01:29 +0530

    Building the APP

--- base/urls.py
+++ base/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('room/<str:pk>/', views.room, name='room'),
     # str value is passed to pk[primary key to link to the database]
     # This will accept requests like http://127.0.0.1:8000/room/1/
-
+    path('create-room/',views.createRoom, name="create-room"),
 ]
